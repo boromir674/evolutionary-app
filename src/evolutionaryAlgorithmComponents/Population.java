179d0d5ba715d8d56f4c2e264d9cedc6b75b20a2
Konstantinos Lampridis <k.lampridis@hotmail.com>
2016-01-25 20:44:22 +0100

    removed implemetation at Population.java

--- src/evolutionaryAlgorithmComponents/Population.java
+++ src/evolutionaryAlgorithmComponents/Population.java
@@ -15,51 +15,58 @@ public class Population {
 
 	private int mu; // population size
 	private int lambda; // number of offsprings to create on every generation
-	private Individual[] mainPopulation; // population
+	private Individual[] pool; // parents and children (offsprings)
 	int generationCount = 1;
 	
-	private Individual[] children;
-	private int popIndex;
+	private int readIndex = 0;
+	private int poolIndex;
+	private int offIndex;
+	private Individual[] parents;
+	private Individual[] survivors;
 
 	public Population(int mu, int lambda){
 		this.mu = mu;
 		this.lambda = lambda;
+		pool = new Individual[mu+lambda];
 		//this.representation = aRepresentation;
 	}
 
 	public void initializeRandom(Representation representation, Random aRandom, EvaluationMethod evaluator) throws Exception{
-		popIndex = 0;
-		mainPopulation = new Individual[mu];
-		children = new Individual[lambda];
+		poolIndex = 0;
+		offIndex = this.mu;
+		pool = new Individual[mu+lambda];
 		generationCount = 1;
 		Individual member;
 		for (int i=0; i<this.mu; i++){
 			member = new Individual();
 			member.initializeRandomly(representation, aRandom);
-			addIndividual(member, evaluator);
+			addIndividualToPool(member, evaluator);
 		}
 	}
 	public int getGenerationCounter(){
 		return generationCount;
 	}
 
-	public void addIndividual(Individual someone, EvaluationMethod evaluator){
+	void addIndividualToPool(Individual someone, EvaluationMethod evaluator){
 		someone.computeFitness(evaluator);
-		mainPopulation[popIndex] = someone;
+		pool[poolIndex] = someone;
+		poolIndex ++;
 	}
-
+	public void setParents(Individual[] parents){
+		this.parents = parents;
+	}
 	public Individual getFittestIndividual(){
 		double[] fitnessArray = util.Util.getFitnessArray(this);
 		int index = util.Util.findMaxIndex(fitnessArray);
-		return mainPopulation[index];
+		return pool[index];
 	}
 
 	public Individual member(int i){
-		return mainPopulation[i];
+		return pool[i];
 	}
 
 	public void set(int i, Individual anIndividual){
-		mainPopulation[i] = anIndividual;
+		pool[i] = anIndividual;
 	}
 
 	/**
@@ -76,16 +83,18 @@ public class Population {
 		return lambda;
 	}
 
-	public Individual[][] getParentsAndChildren() {
-		Individual[][] parentsAndChildren = new Individual[2][];
+	public Individual[] getPool() {
+		/*Individual[][] parentsAndChildren = new Individual[2][];
 		parentsAndChildren[0] = this.mainPopulation;
-		parentsAndChildren[1] = children;
-		return parentsAndChildren;
+		parentsAndChildren[1] = children;*/
+		//return parentsAndChildren;
+		return pool;
 	}
-	
+
 	public void printDiversity() throws Exception {
 		double[] fitArray = Util.getFitnessArray(this);
 		double[] meanAndStd = Util.recursiveMeanAndStd(fitArray);
 		System.out.format("%.2f ", meanAndStd[1]);
 	}
+
 }

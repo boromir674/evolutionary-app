aadd168bf84e7f3112998d31688d2abc5db562f5
Konstantinos Lampridis <k.lampridis@hotmail.com>
2016-01-25 16:46:10 +0100

    refinement

--- src/evolutionaryAlgorithmComponents/Population.java
+++ src/evolutionaryAlgorithmComponents/Population.java
@@ -11,12 +11,15 @@ import java.util.Random;
 import util.Util;
 import evolutionaryAlgorithmComponents.representation.RealValueRepresentation;
 
-public class Population implements Iterable<Individual>{
+public class Population {
 
 	private int mu; // population size
 	private int lambda; // number of offsprings to create on every generation
-	private ArrayList<Individual> pool = new ArrayList<Individual>(); // population
+	private Individual[] mainPopulation; // population
 	int generationCount = 1;
+	
+	private Individual[] children;
+	private int popIndex;
 
 	public Population(int mu, int lambda){
 		this.mu = mu;
@@ -25,7 +28,9 @@ public class Population implements Iterable<Individual>{
 	}
 
 	public void initializeRandom(Representation representation, Random aRandom, EvaluationMethod evaluator) throws Exception{
-		pool.clear();
+		popIndex = 0;
+		mainPopulation = new Individual[mu];
+		children = new Individual[lambda];
 		generationCount = 1;
 		Individual member;
 		for (int i=0; i<this.mu; i++){
@@ -40,25 +45,21 @@ public class Population implements Iterable<Individual>{
 
 	public void addIndividual(Individual someone, EvaluationMethod evaluator){
 		someone.computeFitness(evaluator);
-		pool.add(someone);
-	}
-
-	@Override
-	public Iterator<Individual> iterator() {
-		Iterator<Individual> iPop = pool.iterator();
-		return iPop; 
+		mainPopulation[popIndex] = someone;
 	}
 
 	public Individual getFittestIndividual(){
-		return Collections.max(pool);
+		double[] fitnessArray = util.Util.getFitnessArray(this);
+		int index = util.Util.findMaxIndex(fitnessArray);
+		return mainPopulation[index];
 	}
 
 	public Individual member(int i){
-		return pool.get(i);
+		return mainPopulation[i];
 	}
 
 	public void set(int i, Individual anIndividual){
-		pool.set(i, anIndividual);
+		mainPopulation[i] = anIndividual;
 	}
 
 	/**
@@ -75,13 +76,14 @@ public class Population implements Iterable<Individual>{
 		return lambda;
 	}
 
-	public ArrayList<Individual> getPool() {
-		return pool;
+	public Individual[][] getParentsAndChildren() {
+		Individual[][] parentsAndChildren = new Individual[2][];
+		parentsAndChildren[0] = this.mainPopulation;
+		parentsAndChildren[1] = children;
+		return parentsAndChildren;
 	}
 	
 	public void printDiversity() throws Exception {
-		if (pool.size() != mu)
-			throw new Exception("diversity should be computed only after the survivor selection has been applied");
 		double[] fitArray = Util.getFitnessArray(this);
 		double[] meanAndStd = Util.recursiveMeanAndStd(fitArray);
 		System.out.format("%.2f ", meanAndStd[1]);

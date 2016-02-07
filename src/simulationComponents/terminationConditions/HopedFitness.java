a92f890f5d5e1e971c9dad7e6bb0a1e2b7187e12
Konstantinos Lampridis <k.lampridis@hotmail.com>
2016-02-07 17:04:42 +0100

    implement HopedFitness

--- src/simulationComponents/terminationConditions/HopedFitness.java
+++ src/simulationComponents/terminationConditions/HopedFitness.java
@@ -1,21 +1,36 @@
 package simulationComponents.terminationConditions;
 
+import evolutionaryAlgorithmComponents.AbstractEvaluationMethod;
 import simulationComponents.Experiment;
 
 public class HopedFitness extends AbstractTerminationCondition {
 	
 	private final static String title = "Known/Hoped Fitness";
 	private double hopedFitness;
+	private boolean flag;
 	
 	public HopedFitness(double hopedFitness) {
 		super(title);
 		this.hopedFitness = hopedFitness;
 	}
+	public HopedFitness() {
+		super(title);
+		flag = true;
+	}
 
 	@Override
 	public boolean satisfied(Experiment anExperiment) {
-		/*if (anExperiment.getEvolutionaryAlgorithm().getPopulation().getFittestIndividual().getFitness() >= hopedFitness)
-			return true;*/
+		if (flag){
+			try {
+				hopedFitness = ((AbstractEvaluationMethod) anExperiment.getEvolutionaryAlgorithm().getEvaluator()).getSolutionFitness();
+			} catch (Exception e) {
+				System.exit(0);
+			}
+		}
+		if (anExperiment.getEvolutionaryAlgorithm().getPopulation().getFittestIndividual().
+				getFitness() >= hopedFitness){
+			return true;
+		}
 		return false;
 	}
 }

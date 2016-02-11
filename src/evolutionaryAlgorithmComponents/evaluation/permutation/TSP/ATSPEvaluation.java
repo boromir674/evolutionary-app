0e5b6e7a9c6b3880a9a51c41a9004990c2d2195e
Konstantinos Lampridis <k.lampridis@hotmail.com>
2016-02-11 20:55:58 +0100

    Reorganize ATSPEvaluation

--- src/evolutionaryAlgorithmComponents/evaluation/permutation/TSP/ATSPEvaluation.java
+++ src/evolutionaryAlgorithmComponents/evaluation/permutation/TSP/ATSPEvaluation.java
@@ -20,36 +20,32 @@ public class ATSPEvaluation extends AbstractTSPLIBEvaluation implements ATSP{
 	public double computeFitness(Individual in) {
 		int d = in.getRepresentation().getDimensions();
 		double fitness = 0;
-		int i = 0;
-		while (i<d){
-			try {
-				fitness += this.distance((int)in.getChromosome()[i], (int)in.getChromosome()[(i+1)%d]);
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-			i++;
-		}
+		for (int i=0; i<d; i++)
+			fitness += this.distance((int)in.getChromosome()[i], (int)in.getChromosome()[(i+1)%d]);
 		super.evaluationsUsed ++;
 		return -fitness;
 	}
 
 	@Override
-	public int distance(int node1, int node2) throws Exception {
-		super.checkNodes(node1, node2);
-		if (super.myDistanceCalculator == null) {
+	public int distance(int node1, int node2){
+/*		if (super.myDistanceCalculator == null) {
 			if (super.fullMatrixFlag)
 				return super.fullMatrixDistance(node1, node2);
 			else
 				return (int) super.vector[this.linearIndex(node1-1, node2-1)];
 		}
 		else 
-			return super.myDistanceCalculator.calculateDistance(matrix[node1-1], matrix[node2-1]);
+			return super.myDistanceCalculator.calculateDistance(matrix[node1-1], matrix[node2-1]);*/
+		return super.fullMatrixDistance(node1, node2);
 	}
 
 	@Override
 	public double getSolutionFitness() {
-		// TODO Auto-generated method stub
-		return 0;
+		Integer[] vector = (Integer[])super.getSolutionVector();
+		double fitness = 0;
+		for (int i=0; i<vector.length; i++)
+			fitness += this.distance(vector[i], vector[(i+1)%vector.length]);
+		return -fitness;
 	}
 
 }

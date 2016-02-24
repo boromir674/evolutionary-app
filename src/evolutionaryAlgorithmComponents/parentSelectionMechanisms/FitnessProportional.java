dd66e6c296a6169da7999bb67955765630be77a2
Konstantinos Lampridis <k.lampridis@hotmail.com>
2016-02-24 15:27:32 +0100

    Fix FitnessProportional

--- src/evolutionaryAlgorithmComponents/parentSelectionMechanisms/FitnessProportional.java
+++ src/evolutionaryAlgorithmComponents/parentSelectionMechanisms/FitnessProportional.java
@@ -1,43 +1,27 @@
 package evolutionaryAlgorithmComponents.parentSelectionMechanisms;
 
+import interfaces.FitnessSharingScheme;
+
 import java.util.Random;
 
 import util.Util;
 import evolutionaryAlgorithmComponents.AbstractParentSelection;
+import evolutionaryAlgorithmComponents.AbstractRepresentation;
 import evolutionaryAlgorithmComponents.Individual;
 import evolutionaryAlgorithmComponents.Population;
 
 public class FitnessProportional extends AbstractParentSelection {
-	
+
 	private final static String title = "Fitness Proportional";
-	
+
 	public FitnessProportional(){
 		super(title);
 	}
 
 	@Override
 	public int[] select(Population pop, Random rand) throws Exception {
-		/*double[] fitArray = new double[pop.getMu()];
-		fitArray[0] = pop.getPool()[0].getFitness();
-		double minFitness = fitArray[0];
-		double fitnessSum = fitArray[0];
-		for (int i=1; i<fitArray.length; i++){
-			fitArray[i] = pop.getPool()[i].getFitness();
-			if (fitArray[i] < minFitness)
-				minFitness = fitArray[i];
-			fitnessSum += fitArray[i];
-		}
-		fitnessSum += fitArray.length * (1 - minFitness);
-		double[] cumulProbs = new double[fitArray.length];
-		fitArray[0] = fitArray[0] - minFitness + 1;
-		cumulProbs[0] = fitArray[0]/fitnessSum;
-		for (int i=1; i<fitArray.length; i++) {
-			fitArray[i] = fitArray[i] - minFitness + 1;
-			cumulProbs[i] = cumulProbs[i-1] + fitArray[i]/fitnessSum;
-		}*/
-
-		double[] cumulProbs = util.Util.getCumulativeDistribution(pop.getPool(), 0, pop.getMu());
-		int[] parentPointers = Util.stochasticUniversalSampling(cumulProbs, pop.getLambda(), rand);
+		double[] cumulativeProbabilities = Util.getCumulativeDistribution(pop.getPool(), 0, pop.getMu());
+		int[] parentPointers = Util.stochasticUniversalSampling(cumulativeProbabilities, pop.getLambda(), rand);
 		return parentPointers;
 	}
 

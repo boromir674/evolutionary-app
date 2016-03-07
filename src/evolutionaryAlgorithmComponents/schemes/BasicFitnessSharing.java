89a2cf1970b063f2f7a2c3896fe4679c06047caf
Konstantinos Lampridis <k.lampridis@hotmail.com>
2016-03-07 14:52:12 +0100

    Add parameter σ_share to BasicFitnessSharing

--- src/evolutionaryAlgorithmComponents/schemes/BasicFitnessSharing.java
+++ src/evolutionaryAlgorithmComponents/schemes/BasicFitnessSharing.java
@@ -14,9 +14,14 @@ public class BasicFitnessSharing extends AbstractFitnessSharingScheme {
 
 	private final static String title = "Simple fitness sharing";
 	private int alpha = 1; //linear recommended
+	protected double sigmaShare = 5;
 	
 	public BasicFitnessSharing() {
 		super(title);
+	}
+	public BasicFitnessSharing(double sigmaShare) {
+		super(title);
+		this.sigmaShare = sigmaShare;
 	}
 
 	@Override
@@ -24,8 +29,8 @@ public class BasicFitnessSharing extends AbstractFitnessSharingScheme {
 		double denominator = 0;
 		for (int i=0; i<ind.getPopulation().getMu(); i++) {
 			double distance = ((AbstractRepresentation)ind.getPopulation().getPool()[0].getRepresentation()).genotypicDistance(ind, ind.getPopulation().getPool()[i]);
-			if (distance <= 5)
-				denominator += 1 - Math.pow(distance/5, alpha);
+			if (distance <= sigmaShare)
+				denominator += 1 - Math.pow(distance/sigmaShare, alpha);
 		}
 		return ind.getFitness() / denominator;
 	}

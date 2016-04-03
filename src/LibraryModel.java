13931cde3896ee708e15a40c6f265ea2c9a1e816
Konstantinos Lampridis <k.lampridis@hotmail.com>
2016-04-03 19:45:57 +0200

    Create LibraryModel.java

---
+++ src/LibraryModel.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+
+public final class LibraryModel {
+	
+	private final ArrayList<String> parentSelectionModels = new ArrayList<String>();
+	private final ArrayList<String> survivorSelectionModels = new ArrayList<String>();
+	private final ArrayList<String> recombinationModels = new ArrayList<String>();
+	private final ArrayList<String> mutationModels = new ArrayList<String>();
+	private final String[] folders = new String[]{"evaluation/realValueEvaluations/"}; 
+	
+//	public LibraryObserver(){
+		
+	//}
+	
+}

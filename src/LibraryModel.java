21784fdf0f24a9ea13495b57fad80d1fd7f9b737
Konstantinos Lampridis <k.lampridis@hotmail.com>
2016-04-03 20:22:06 +0200

    Create constructor

--- src/LibraryModel.java
+++ src/LibraryModel.java
@@ -1,16 +1,27 @@
+import java.io.File;
 import java.util.ArrayList;
-
+import java.util.Arrays;
 
 public final class LibraryModel {
 	
-	private final ArrayList<String> parentSelectionModels = new ArrayList<String>();
-	private final ArrayList<String> survivorSelectionModels = new ArrayList<String>();
-	private final ArrayList<String> recombinationModels = new ArrayList<String>();
+	/*private final ArrayList<String> recombinationModels = new ArrayList<String>();
 	private final ArrayList<String> mutationModels = new ArrayList<String>();
-	private final String[] folders = new String[]{"evaluation/realValueEvaluations/"}; 
+	private final ArrayList<String> parentSelectionModels = new ArrayList<String>();
+	private final ArrayList<String> survivorSelectionModels = new ArrayList<String>();*/
 	
-//	public LibraryObserver(){
+	private final String[] folders = new String[]{"evaluation/realValueEvaluations/",
+			"variationOperators/recombination","variationOperators/mutation","parentSelectionMechanisms/","survivorSelectionMechanisms"};
+
+	private final ArrayList[] models = new ArrayList[folders.length];
+	
+	public LibraryModel(){
 		
-	//}
+		for (int i=0; i<folders.length; i++) {
+			String path = System.getProperty("user.dir")+"/evolutionaryAlgorithmComponents/" + folders[i];
+			File file = new File(path);
+			models[i] = new ArrayList<String>(Arrays.asList(file.list()));
+		}
+		
+	}
 	
 }

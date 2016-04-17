3850f7399783f0c7f21c6abb76f599d5890c2a52
Konstantinos Lampridis <k.lampridis@hotmail.com>
2016-04-17 04:08:35 +0200

    Implement Evaluation, Parent/Survivor

--- src/util/HyperFactory.java
+++ src/util/HyperFactory.java
@@ -3,9 +3,12 @@
  */
 package util;
 
+import evolutionaryAlgorithmComponents.evaluation.permutation.TSP.TSPProblemFactory;
 import interfaces.EvaluationMethod;
+import interfaces.Mutation;
 import interfaces.ParentSelection;
 import interfaces.Recombination;
+import interfaces.Representation;
 import interfaces.SurvivorSelection;
 
 /**
@@ -14,30 +17,42 @@ import interfaces.SurvivorSelection;
  */
 public final class HyperFactory {
 
-
-	public final static EvaluationMethod getEvaluationMethod(String filename){
-
-	}
-
-	public final static ParentSelection getParentSelection(String fileName){
+	public final static EvaluationMethod getEvaluationMethod(String fileName) throws Exception{
 		String s = "evolutionaryAlgorithmComponents.evaluation.realValueFunction.";
-		Class.forName(s+fileName.substring(0, fileName.length()-5)).newInstance();
+		if (fileName.endsWith(".java"))
+			return (EvaluationMethod) Class.forName(s+fileName.substring(0, fileName.length()-5)).newInstance();
+		else {
+			TSPProblemFactory fac = new TSPProblemFactory();
+			return fac.produceTSPProblem(fileName);
+		}
 	}
 
-	public final static SurvivorSelection getSurvivorSelection(String filename){
+	public final static ParentSelection getParentSelection(String fileName) throws InstantiationException, IllegalAccessException, ClassNotFoundException{
+		String s = "evolutionaryAlgorithmComponents.parentSelectionMechanisms.";
+		return (ParentSelection) Class.forName(s+fileName.substring(0, fileName.length()-5)).newInstance();
+	}
+
+	public final static SurvivorSelection getSurvivorSelection(String fileName) throws InstantiationException, IllegalAccessException, ClassNotFoundException{
+		String s = "evolutionaryAlgorithmComponents.survivorSelectionMechanisms.";
+		return (SurvivorSelection) Class.forName(s+fileName.substring(0, fileName.length()-5)).newInstance();
+	}
+
+	public final static Recombination getCrossoverOperator(String filename, double prob){
+		return null;
+
+	}
+	public final static Mutation getMutationOperator(String filename, double prob){
+		return null;
 
 	}
 
-	public final static Recombination getCrossoverOperator(String filename){
+	public final static Representation getRepresentation(EvaluationMethod anEvaluationMethod){
+		return null;
 
 	}
-	public final static Mutation getMutationOperator(String filename){
+	public final static Representation getPermutationRepresentation(int numberOfNodes){
+		return null;
 
 	}
-	
-	public final static Representation getRepresentation(String filename){
-		
-	}
-	
-	
+
 }

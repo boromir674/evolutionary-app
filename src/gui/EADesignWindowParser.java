6f4e6aed2a6ce2d12a9b25ce8144181393a36e2a
Konstantinos Lampridis <k.lampridis@hotmail.com>
2016-04-20 12:59:56 +0200

    Implement parse EA

--- src/gui/EADesignWindowParser.java
+++ src/gui/EADesignWindowParser.java
@@ -2,14 +2,18 @@ package gui;
 
 import java.lang.reflect.InvocationTargetException;
 
+import util.HyperFactory;
 import interfaces.EvaluationMethod;
 import interfaces.Mutation;
 import interfaces.ParentSelection;
 import interfaces.Recombination;
+import interfaces.Representation;
 import interfaces.SurvivorSelection;
 import evolutionaryAlgorithmComponents.EvolutionaryAlgorithm;
 import evolutionaryAlgorithmComponents.Population;
+import evolutionaryAlgorithmComponents.VarianceOperator;
 import evolutionaryAlgorithmComponents.evaluation.permutation.TSP.TSPProblemFactory;
+import evolutionaryAlgorithmComponents.representation.PermutationRepresentation;
 
 public class EADesignWindowParser {
 	
@@ -24,14 +28,18 @@ public class EADesignWindowParser {
 	
 	@SuppressWarnings("unused")
 	public EvolutionaryAlgorithm parse() throws Exception{ 
-		EvaluationMethod evaluation = this.parseEvaluation();
+		EvaluationMethod evaluation = HyperFactory.getEvaluationMethod(window.lblProblemInstance.getText());
 		Population population = new Population(Integer.parseInt(window.muJTextField.getText()), Integer.parseInt(window.lambdaJTextField.getText()));
-		Recombination recombination = (Recombination) Class.forName(window.recombinationJComboBox.getSelectedItem().toString()).getConstructor(null).newInstance(null); 
-		Mutation mutation = (Mutation) Class.forName(window.mutationJComboBox.getSelectedItem().toString()).getConstructor(double.class).newInstance(Integer.parseInt(window.recombinationRateJTextField.getText()));
-		ParentSelection parentSelection = (ParentSelection) Class.forName(window.parentSelectionList.getSelectedItem().toString()).newInstance();
-		SurvivorSelection survivorSelection = (SurvivorSelection) Class.forName(window.survivorSelectionList.getSelectedItem().toString()).newInstance();
-		//Representation representation = 
-		return null;
+		Recombination recombination = HyperFactory.getCrossoverOperator(window.recombinationJComboBox.getSelectedItem().toString(), Integer.parseInt(window.recombinationRateJTextField.getText()));  
+		Mutation mutation = HyperFactory.getMutationOperator(window.mutationJComboBox.getSelectedItem().toString(), Integer.parseInt(window.recombinationRateJTextField.getText()));
+		ParentSelection parentSelection = HyperFactory.getParentSelection(window.parentSelectionList.getSelectedItem().toString());
+		SurvivorSelection survivorSelection = HyperFactory.getSurvivorSelection(window.survivorSelectionList.getSelectedItem().toString());
+		Representation representation;
+		if (window.lblProblemInstance.getText().contains(".java"))
+			representation = HyperFactory.getRepresentation(window.lblProblemInstance.getText());
+		else 
+			representation = HyperFactory.getRepresentation(window.dJTextField.getText(), window.mutationJComboBox.getSelectedItem().toString());
+		return new EvolutionaryAlgorithm(representation, evaluation, population, parentSelection, new VarianceOperator(recombination, mutation), survivorSelection);
 		
 	}
 	
